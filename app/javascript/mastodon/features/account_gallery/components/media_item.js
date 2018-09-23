f8cc129f269d3b829778c5f5c8de6bf06f208a03
noellabo <noel.yoshiba@gmail.com>
2018-09-23 11:37:51 +0900

    fixup! feature-media-action-toolbar

--- app/javascript/mastodon/features/account_gallery/components/media_item.js
+++ app/javascript/mastodon/features/account_gallery/components/media_item.js
@@ -3,16 +3,26 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import ImmutablePureComponent from 'react-immutable-pure-component';
 import Permalink from '../../../components/permalink';
 import { displaySensitiveMedia } from '../../../initial_state';
+import { me } from '../../../iinitial_state';
+
+const messages = defineMessages({
+  favourite: { id: 'status.favourite', defaultMessage: 'Favourite' },
+});
 
 export default class MediaItem extends ImmutablePureComponent {
 
   static propTypes = {
+    onFavourite: PropTypes.func,
     media: ImmutablePropTypes.map.isRequired,
   };
 
   state = {
     visible: !this.props.media.getIn(['status', 'sensitive']) || displaySensitiveMedia,
   };
+
+  handleFavouriteClick = () => {
+    this.props.onFavourite(this.props.status);
+  }
 
   handleClick = () => {
     if (!this.state.visible) {
@@ -32,6 +42,7 @@ export default class MediaItem extends ImmutablePureComponent {
     const x = ((focusX /  2) + .5) * 100;
     const y = ((focusY / -2) + .5) * 100;
     const style = {};
+    const anonymousAccess    = !me;
 
     let label, icon;
 
@@ -56,6 +67,9 @@ export default class MediaItem extends ImmutablePureComponent {
           {icon}
           {label}
         </Permalink>
+        <div className='account-gallery__action-bar'>
+          <IconButton className='account-gallery__action-bar-button star-icon' disabled={anonymousAccess} animate active={status.get('favourited')} pressed={status.get('favourited')} title={intl.formatMessage(messages.favourite)} icon='star' onClick={this.handleFavouriteClick} />
+        </div>
       </div>
     );
   }
